58613e0d2a9b7cc77768fb1636a4545edf26282d
Mauricio Cendales <malexcdl@hotmail.com>
2021-09-14 17:21:36 -0500

    Se agrega un archivo de presentación en Java

---
+++ Mauricio/src/Mauricio.java
@@ -0,0 +1,6 @@
+public class Mauricio {
+   public static void main(String[] args) {
+            System.out.println("Mi nombre es Mauricio Cendales, en esta semana estoy asumiendo el rol de Desarrollador en el grupo Code Cleaners");
+   }
+
+}
